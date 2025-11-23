e4597b62043437a53237980c99793644d88a7dc7
ChrisMicMad <123459412+ChrisMicMad@users.noreply.github.com>
2025-11-23 21:20:11 +0100

    fix

--- services/loader_testing/src/api-service/api/main.py
+++ services/loader_testing/src/api-service/api/main.py
@@ -20,7 +20,7 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 logger.info(f"Logging to file: {log_file}")
-# =============================================================
+# ===========================================================
 
 app = FastAPI()
 
